9d17b520e9de1ecd89e31bdc4a9fed11064cb798
Acacio Magalhães <ama@como-dl.com>
2024-05-27 13:04:02 +0200

    initial commit

---
+++ eks.tf
@@ -0,0 +1,65 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.11.1"
+
+  cluster_name                   = local.name
+  cluster_endpoint_public_access = true
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.intra_subnets
+
+  enable_cluster_creator_admin_permissions = true
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    ami_type       = "AL2_x86_64"
+    instance_types = ["m5.large"]
+  }
+
+  eks_managed_node_groups = {
+    amc-cluster-wg = {
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+
+      instance_types = ["t3.large"]
+      capacity_type  = "SPOT"
+    }
+  }
+
+# Added to try to solve Istio issue
+
+    node_security_group_additional_rules = {
+    ingress_15017 = {
+      description                   = "${local.name} Cluster API - Istio Webhook namespace.sidecar-injector.istio.io"
+      protocol                      = "TCP"
+      from_port                     = 15017
+      to_port                       = 15017
+      type                          = "ingress"
+      source_cluster_security_group = true
+    }
+    ingress_15012 = {
+      description                   = "${local.name} Cluster API to nodes ports/protocols"
+      protocol                      = "TCP"
+      from_port                     = 15012
+      to_port                       = 15012
+      type                          = "ingress"
+      source_cluster_security_group = true
+    }
+  }
+
+  tags = local.tags
+}
---
+++ provider.tf
@@ -0,0 +1,16 @@
+locals {
+  region = "eu-central-1"
+  name   = "dev-test-cluster"
+  vpc_cidr = "172.0.0.0/16"
+  azs      = ["eu-central-1a", "eu-central-1b"]
+  public_subnets  = ["172.0.1.0/24", "172.0.2.0/24", "172.0.3.0/24"]
+  private_subnets = ["172.0.101.0/24", "172.0.102.0/24", "172.0.103.0/24"]
+
+  tags = {
+    Example = local.name
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
